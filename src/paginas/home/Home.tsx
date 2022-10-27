c4d17c729bd49e50ea8c0b157864418341bc11ed
ScarletMartins <scarletgam@icloud.com>
2022-10-27 14:26:26 -0300

    Inicio do uso de React.js para o desenvolvimento do projeto Blog Pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+         <h1 className='titulo'>Home</h1>
+         <img className='img' src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-entusiasmado_114360-3478.jpg?w=900&t=st=1666891265~exp=1666891865~hmac=2080e4dd6762ae518a30a686e42b2be496cf5e5b021db01fcd9d6d8d95f1c2e1" 
+         alt="Imagem utensílios de viagem" />
+        </>
+    );
+}
+
+export default Home;
